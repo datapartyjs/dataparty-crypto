91eb46555b79f5091f4277881e661f2c40ba5f39
mukejane <834449672@qq.com>
2026-06-08 01:04:02 +0800

    fix typo: rename pqSignSLDSeed to pqSignSLHSeed

--- src/routines.ts
+++ src/routines.ts
@@ -179,7 +179,7 @@ export const createKey = async (
 
     const pqKemSeed = await hkdf('sha512', seed, HkdfFullseedSalt, 'pqkem', pqkemClass.lengths.seed)
     const pqSignMLSeed = await hkdf('sha512', seed, HkdfFullseedSalt, 'pqsignml', pqsignmlClass.lengths.seed)
-    const pqSignSLDSeed = await hkdf('sha512', seed, HkdfFullseedSalt, 'pqsignslh', pqsignslhClass.lengths.seed)
+    const pqSignSLHSeed = await hkdf('sha512', seed, HkdfFullseedSalt, 'pqsignslh', pqsignslhClass.lengths.seed)
   
   
     const pqKemKeyPair = pqkemClass.keygen( pqKemSeed );
